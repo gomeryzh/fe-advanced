517b64530ab0651333f2bafb4460e94954f0a4bf
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2018-05-06 22:53:43 +0300

    module-03 done after refactoring

--- module-03/js/index.js
+++ module-03/js/index.js
@@ -1,37 +1,30 @@
 'use strict';
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 const addLogin = (login, logins) => {
   let loginValidity = checkLoginValidity(login);
   let loginExist = checkIfLoginExists(login, logins);
   if (loginValidity) {
     if (loginExist) {
-      alert('Такой логин уже используется!');
-    } else {
-      logins.push(login);
-      alert('Логин успешно добавлен!');
+      alert("Такой логин уже используется!");
+      return;
     }
-  } else {
-    alert('Ошибка! Логин должен быть от 4 до 16 символов');
+    logins.push(login);
+    alert("Логин успешно добавлен!");
+    return;
   }
+  alert("Ошибка! Логин должен быть от 4 до 16 символов");
+  return;
 };
-const checkLoginValidity = (login) => {
-  let result
+const checkLoginValidity = login => {
   if (login.length < 4 || login.length > 16) {
-    result = false;
-    return result;
-  } else {
-    result = true;
-    return result;
+    return false;
   }
+  return true;
 };
 const checkIfLoginExists = (login, logins) => {
-  let result
   if (logins.includes(login)) {
-    result = true;
-    return result;
-  } else {
-    result = false;
-    return result;
+    return true;
   }
+  return false;
 };
-addLogin('somelogin', logins);
+addLogin("Mango", logins);
