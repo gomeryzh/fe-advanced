c7442ecd9649100cd201071431de67d348c2f312
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2018-05-13 00:20:41 +0300

    need to fix serve method

--- module-04/js/index.js
+++ module-04/js/index.js
@@ -18,33 +18,43 @@ const order = {
 
 const Cashier = function ({
     name = 'cashier',
-    products = {},
+    products = {
+        bread: 10,
+        milk: 15,
+        apples: 20,
+        chicken: 50,
+        pork: 80,
+        cheese: 60,
+        tea: 20,
+        candy: 25
+    },
     totalPrice = 0,
     customerMoney = 0,
-    changeamount = 0
+    changeAmount = 0
 }) {
     this.name = name;
     this.products = products;
     this.totalPrice = totalPrice;
     this.customerMoney = customerMoney;
-    this.changeamount = changeamount;
+    this.changeAmount = changeAmount;
 
     this.countTotalPrice = function (order) {
         let keyToFind;
         let multiply;
+        let orderPrice = 0;
         for (let elem in order) {
-          keyToFind = elem;
-          for (let key in products) {
-            const hasOwnKey = products.hasOwnProperty(key);
-            const isMatch = key === keyToFind;
-            if (hasOwnKey && isMatch) {
-              let multiply = order[elem] * products[key];
-              console.log(multiply);
-              this.totalPrice += multiply;
+            keyToFind = elem;
+            for (let key in this.products) {
+                const hasOwnKey = this.products.hasOwnProperty(key);
+                const isMatch = key === keyToFind;
+                if (hasOwnKey && isMatch) {
+                    let multiply = order[elem] * this.products[key];
+                    console.log(multiply);
+                    orderPrice += multiply;
+                }
             }
-          }
         }
-        return this.totalPrice;
+        this.totalPrice = orderPrice;
     };
 
     this.getCustomerMoney = function () {
@@ -52,43 +62,60 @@ const Cashier = function ({
         let isNumber;
         let inRange;
         let validInput;
+
         do {
             clientInput = prompt(`Стоимость Вашего заказа ${this.totalPrice}, оплатите товар:`);
             const clientInputAsNumber = Number(clientInput);
-            isNumber = Number.isNaN(clientInputAsNumber);
+            isNumber = !Number.isNaN(clientInputAsNumber);
             inRange = clientInputAsNumber >= this.totalPrice;
             validInput = isNumber && inRange;
-            if (customerMoney === null) {
-                return null;
+            if (clientInput === null) {
+                this.customerMoney = null;
+                return;
             }
             if (validInput) {
-               this.customerMoney = clientInputAsNumber;
-               return
+                this.customerMoney = clientInputAsNumber;
+                return
             }
             alert('недостаточно денег, пробуй еще');
-        } while (!validInput)
-       
+        } while (!validInput);
     };
 
     this.countChange = function () {
-        changeamount = customerMoney - totalPrice;
+        this.changeAmount = this.customerMoney - this.totalPrice;
+        return this.changeAmount;
     };
 
     this.reset = function () {
-        totalPrice = 0;
-        customerMoney = 0;
-        changeAmount = 0;
+        this.totalPrice = 0;
+        this.customerMoney = 0;
+        this.changeAmount = 0;
     };
-    
+
     this.serve = function (order) {
         this.countTotalPrice(order);
         this.getCustomerMoney();
         this.countChange();
-        if (this.changeAmount > 0) {
-            alert(`Спасибо за покупку, ваша сдача ${this.changeAmount}`)
-        }
-        alert('Очень жаль, что-то пошло не так, приходите еще');
+        // if (this.customerMoney !== null && this.customerMoney > this.totalPrice) {
+        //     this.countChange();
+        //     alert(`Спасибо за покупку, ваша сдача ${this.changeAmount}`);
+        // }
+        // alert('Очень жаль, что-то пошло не так, приходите еще');
         this.reset();
 
     };
 };
+
+
+
+const vova = new Cashier('Vova', products, 0, 0, 0);
+console.log(vova);
+
+// vova.countTotalPrice(order);
+// console.log(vova);
+
+// vova.getCustomerMoney();
+// console.log(vova);
+
+vova.serve(order);
+console.log(vova);
